b44977bf12ad334cd46dfb3df2c32836b97c592f
Olena Dovbenchuk <dovbenchuk.pn@ucu.edu.ua>
2025-04-06 12:50:27 +0300

    add the solution to the task "Implement Queue using Stacks" and photo tests_passed.png

---
+++ Implement Queue using Stacks/task_1.py
@@ -0,0 +1,40 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack_in.append(x)
+
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out.pop()
+
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out[-1]
+
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.stack_in and not self.stack_out
